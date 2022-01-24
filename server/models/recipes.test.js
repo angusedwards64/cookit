258831025961a52dc6a2d397b3470a3a479ac0a3
xReubeNx <reuben@reubenhill.net>
2022-01-24 17:14:31 +0000

    new branch

---
+++ server/models/recipes.test.js
@@ -0,0 +1,9 @@
+const { getById, list, add, edit, deleteById } = require('./recipes.js');
+
+test('tests should be working', () => {
+  expect(1).toBe(1);
+});
+
+test("should be able to add and remove recipes", () => {
+
+})
